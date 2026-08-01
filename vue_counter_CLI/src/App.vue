fb360cbe77ab3b9fd283d82dfbc8df8aaa451616
Kiarash Haghi <kiarash90haghi@gmail.com>
2026-08-02 00:30:49 +0330

    create a counter app with CLI

---
+++ vue_counter_CLI/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    let counter = ref(0)
+
+
+</script>
+
+<template>
+  <h1 id="heading">Counter is: {{ counter }}</h1>
+
+  <button id="increment" @click="counter++">+</button>
+  <button id="decrement" @click="counter--">-</button>
+</template>
+
+<style scoped>
+
+</style>
